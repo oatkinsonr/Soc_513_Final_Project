65771574deb4fb9d7e92d9d30a6d34732e460e33
Olivia Atkinson <oatkins2@uoregon.edu>
2019-05-16 10:50:13 -0700

    state mm data

--- analysis/organize_data.R
+++ analysis/organize_data.R
@@ -1,6 +1,6 @@
 #' ---
 #' title: "organize_data.R"
-#' author: ""
+#' author: "Olivia Atkinson"
 #' ---
 
 # This script will read in raw data from the input directory, clean it up to produce 
@@ -8,3 +8,15 @@
 
 #source in any useful functions
 source("useful_functions.R")
+
+
+mm_data <- read.table("census_region_mm.txt", header = TRUE, sep = ",")
+docgender_data <- read.csv("doc_gender.csv")
+
+# 
+
+state_mm <- read_tsv("state_mm_year.txt", na=c("","NA","Suppressed"))
+
+state_mm <- subset(state_mm, !is.na(Deaths))
+
+table(state_mm$State, state_mm$Year)
