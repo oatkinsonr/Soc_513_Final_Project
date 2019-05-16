c61d6aa175d48cd4af1413b74371ae21dd12fdb5
Aaron Gullickson <aarong@uoregon.edu>
2019-05-16 11:05:19 -0700

    percent doctors by region

--- analysis/organize_data.R
+++ analysis/organize_data.R
@@ -8,14 +8,24 @@
 
 #source in any useful functions
 source("useful_functions.R")
+library(readr)
 
+mm_data <- read.table("input/census_region_mm.txt", header = TRUE, sep = ",")
+docgender_data <- read.csv("input/doc_gender.csv")
 
-mm_data <- read.table("census_region_mm.txt", header = TRUE, sep = ",")
-docgender_data <- read.csv("doc_gender.csv")
-
+tab <- table(docgender_data$USCPRSEX, docgender_data$REGMEPSRD,
+             docgender_data$YEAR)[2:3,,]
+tab <- prop.table(tab, c(2,3))
+dimnames(tab) <- list(c("Male","Female"),
+                      c("NIU","NE","MW","S","W"),
+                      2010:2016)
+doctor_female <- as.data.frame.table(tab)
+colnames(doctor_female) <- c("gender","region","year","percent_female")
+doctor_female <- subset(doctor_female, gender=="Female",
+                        select=c("region","year","percent_female"))
 # 
 
-state_mm <- read_tsv("state_mm_year.txt", na=c("","NA","Suppressed"))
+state_mm <- read_tsv("input/state_mm_year.txt", na=c("","NA","Suppressed"))
 
 state_mm <- subset(state_mm, !is.na(Deaths))
 
