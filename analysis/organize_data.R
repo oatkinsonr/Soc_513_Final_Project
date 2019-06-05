463ac8700c778f1539940514a630173bf24ff348
Olivia Atkinson <oatkins2@uoregon.edu>
2019-06-05 13:00:28 -0700

    cleaned data and moved to output

--- analysis/organize_data.R
+++ analysis/organize_data.R
@@ -48,8 +48,9 @@ colnames(birth_data) <- c("region","year","births")
 mm <- merge(mm_data, birth_data)
 mm$mmr <- (mm$deaths/mm$births)
 
-----------------------------------------------------------------------------------------
-#Doctor Gender Data 
+
+# Doc Gender Data ---------------------------------------------------------
+
 docgender_data <- read.csv("input/doc_gender.csv")
 
 tab <- table(docgender_data$USCPRSEX, docgender_data$REGMEPSRD,
@@ -62,6 +63,16 @@ doctor_female <- as.data.frame.table(tab)
 colnames(doctor_female) <- c("gender","region","year","percent_female")
 doctor_female <- subset(doctor_female, gender=="Female" & region!="NIU",
                         select=c("region","year","percent_female"))
+doctor_female$percent_female <- 100*doctor_female$percent_female
+
+mm <- merge(mm, doctor_female, all.x=TRUE, all.y=FALSE)
+
+# Save Data ---------------------------------------------------------------
+
+#drop 2017
+mm <- subset(mm, year<2017)
+
+save(mm, file="output/analytical_data.RData")
 
 
 
